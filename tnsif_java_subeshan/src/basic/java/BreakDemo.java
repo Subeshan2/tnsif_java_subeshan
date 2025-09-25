32613eee42fa6c7484aa302d733500a0de145bca
Subeshan2 <nsubeshan22@gmail.com>
2025-09-25 07:33:59 +0530

    this the sample

--- tnsif_java_subeshan/src/basic/java/BreakDemo.java
+++ tnsif_java_subeshan/src/basic/java/BreakDemo.java
@@ -1,14 +1,14 @@
-package basic.java;
+//Program to demonstrate  break statement
+package day1.basics;
 
-public class BasicJava {
-
+public class BreakDemo {
 	public static void main(String[] args) {
+
 		for (int i = 5; i < 10; i++) {
 			if (i == 5)
 				break;
 			System.out.println(i);
 		}
-		
-	}
 
+	}
 }
